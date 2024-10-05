c092f144c526332e5562da644c33b6732bf4fc82
dragoncastle <alemfla3181@naver.com>
2024-10-05 23:08:51 +0900

    first commit

---
+++ src/main/java/com/thehecklers/sburredis/SburredisApplication.java
@@ -0,0 +1,13 @@
+package com.thehecklers.sburredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SburredisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SburredisApplication.class, args);
+	}
+
+}
